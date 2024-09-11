c82313de1b7754e8dbd925a0c5b50e18c06b30bc
lelelol <leandrofortunatojf@gmail.com>
2024-09-11 16:54:42 -0300

    front inicial login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="bg-red-900">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+          <div className="flex justify-center itens-center bg-blue-400 min-w-[100vw] min-h-screen">
+            <div className='gap-8 flex justify-center items-center  min-w-[100vw] flex-col'>
+              <h1 className='text-4xl'>Login</h1>
+            <form className='flex flex-col items-start min-w-[20%] bg-white p-4 rounded-md'>
+              <label>
+                Usuario: </label> 
+                <input className='bg-gray-300 rounded-md w-full' type="text" />
+              
+              <br />
+              <label>
+                Senha:</label>  
+                <input className='bg-gray-300 rounded-md w-full' type="password" />
+              
+              <br />
+              <button type="submit" className='self-center bg-gray-300 p-2 pl-4 pr-4 rounded-md'>Login</button>
+              </form>
+            </div>
+          </div>
   );
 }
 
